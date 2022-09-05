1dabf37f993b226574efc621f33da129068176ce
Gunal Sadic <gunal.sadic@totalsoft.ro>
2022-09-05 12:03:17 +0300

    Adaugat pachet nou Nuggets pentru include

---
+++ Concediu_WebApi/Controllers/AngajatiController.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Concediu_WebApi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AngajatiController : ControllerBase
+//    {
+//        public List<Angajati> GetAngajati
+//    }
+//}
