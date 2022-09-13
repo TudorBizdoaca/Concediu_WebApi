3b3ca423fd5127d9d07d7a8232cf8af5346f0dd3
Tudor Bizdoaca <tudor.bizdoaca@totalsoft.ro>
2022-09-13 14:06:22 +0300

    dada

--- Concediu_WebApi/Controllers/TabelConcediiController.cs
+++ Concediu_WebApi/Controllers/TabelConcediiController.cs
@@ -17,22 +17,59 @@ namespace Concediu_WebApi.Controllers
         }
 
         [HttpGet("GetNrConcedii")]
-        public int GetNrConcedii()
+        public int GetNrConcedii(bool esteAdmin , int id)
         {
-            return _context.Concedius.Count();
+            if (esteAdmin == false)
+            {
+                return _context.Concedius.Include(x => x.Angajat).Where(x => x.Angajat.Id == id).Count();
+            }
+            else
+            {
+                return _context.Concedius.Count();
+            }
+           
         }
 
         [HttpGet("GetConcedii")]
-        public List<Concediu> GetConcedii(int position)
+        public List<Concediu> GetConcedii(int position, bool esteAdmin, int id)
         {
-            var nextPage = _context.Concedius.Include(x => x.Angajat)
+            if (esteAdmin == false)
+            {
+                var nextPage = _context.Concedius.Include(x => x.Angajat)
                 .Include(x => x.StareConcediu)
                 .Include(x => x.TipConcediu)
                 .Select(x => new Concediu
-                { Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                {
+                    Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat {Id = x.Angajat.Manager.Id , Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume  } },
                     TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
                     Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
-                    DataInceput = x.DataInceput, DataSfarsit = x.DataSfarsit,
+                    DataInceput = x.DataInceput,
+                    DataSfarsit = x.DataSfarsit,
+                    StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                    Id = x.Id
+
+
+                })
+                .Where(x=> x.Angajat.Manager.Id == id)
+                .OrderBy(x => x.Id)
+                .Skip(position)
+                .Take(10)
+                .ToList();
+
+                return nextPage;
+            }
+            else
+            {
+                var nextPage = _context.Concedius.Include(x => x.Angajat)
+                .Include(x => x.StareConcediu)
+                .Include(x => x.TipConcediu)
+                .Select(x => new Concediu
+                {
+                    Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                    TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                    Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                    DataInceput = x.DataInceput,
+                    DataSfarsit = x.DataSfarsit,
                     StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
                     Id = x.Id
 
@@ -42,28 +79,52 @@ namespace Concediu_WebApi.Controllers
                 .Take(10)
                 .ToList();
 
-            return nextPage;
+                return nextPage;
+            }
+            
         }
 
 
         [HttpGet("GetConcediiByStareId")]
-        public List<Concediu> GetConcediiByStareId(int stareId)
+        public List<Concediu> GetConcediiByStareId(int stareId, bool esteAdmin, int id)
         {
-            return _context.Concedius.Include(x => x.Angajat)
-              .Include(x => x.StareConcediu)
-              .Include(x => x.TipConcediu).Where(x => x.StareConcediuId == stareId)
-              .Select(x => new Concediu
-              {
-                  Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
-                  TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
-                  Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
-                  DataInceput = x.DataInceput,
-                  DataSfarsit = x.DataSfarsit,
-                  StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
-                  Id = x.Id
+            if (esteAdmin == false)
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+             .Include(x => x.StareConcediu)
+             .Include(x => x.TipConcediu).Where(x => x.StareConcediuId == stareId)
+             .Select(x => new Concediu
+             {
+                 Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume, Id = x.Angajat.Manager.Id } },
+                 TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                 Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                 DataInceput = x.DataInceput,
+                 DataSfarsit = x.DataSfarsit,
+                 StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                 Id = x.Id
 
-              })
-              .ToList();
+             })
+             .Where(x => x.Angajat.Manager.Id == id)
+             .ToList();
+            }
+            else
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+             .Include(x => x.StareConcediu)
+             .Include(x => x.TipConcediu).Where(x => x.StareConcediuId == stareId)
+             .Select(x => new Concediu
+             {
+                 Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                 TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                 Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                 DataInceput = x.DataInceput,
+                 DataSfarsit = x.DataSfarsit,
+                 StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                 Id = x.Id
+
+             })
+             .ToList();
+            }
         }
 
 
@@ -75,10 +136,30 @@ namespace Concediu_WebApi.Controllers
 
 
         [HttpGet("GetConcediiDupaNumeAngajat")]
-        public List<Concediu> GetConcediiDupaNumeAngajat(string nume)
+        public List<Concediu> GetConcediiDupaNumeAngajat(string nume, bool esteAdmin, int id)
         {
+            if (esteAdmin == false)
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+               .Include(x => x.StareConcediu)
+               .Include(x => x.TipConcediu).Where(x => x.Angajat.Nume.Contains(nume))
+               .Select(x => new Concediu
+               {
+                   Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume, Id = x.Angajat.Manager.Id } },
+                   TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                   Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                   DataInceput = x.DataInceput,
+                   DataSfarsit = x.DataSfarsit,
+                   StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                   Id = x.Id
 
-            return _context.Concedius.Include(x => x.Angajat)
+               })
+               .Where(x => x.Angajat.Manager.Id == id)
+               .ToList();
+            }
+            else
+            {
+                return _context.Concedius.Include(x => x.Angajat)
                .Include(x => x.StareConcediu)
                .Include(x => x.TipConcediu).Where(x => x.Angajat.Nume.Contains(nume))
                .Select(x => new Concediu
@@ -93,46 +174,82 @@ namespace Concediu_WebApi.Controllers
 
                })
                .ToList();
+            }
+           
 
         }
 
         [HttpGet("GetConcediiIntreDataInceputSiDataFinal")]
-        public List<Concediu> GetConcediiIntreDataInceputSiDataFinal(DateTime dataInceput, DateTime dataFinal)
+        public List<Concediu> GetConcediiIntreDataInceputSiDataFinal(DateTime dataInceput, DateTime dataFinal, bool esteAdmin, int id)
         {
             List<Concediu> listaReturnata = new List<Concediu>();
 
-            if (dataInceput <= dataFinal)
+            if (esteAdmin == false)
             {
-                listaReturnata = _context.Concedius.Include(x => x.Angajat)
-               .Include(x => x.StareConcediu)
-               .Include(x => x.TipConcediu)
-               .Where(x => (x.DataInceput.Date >= dataInceput.Date && x.DataInceput.Date <= dataFinal.Date
-                                    && x.DataSfarsit.Date >= dataInceput.Date && x.DataSfarsit.Date <= dataFinal.Date))
-               .Select(x => new Concediu
-               {
-                   Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
-                   TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
-                   Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
-                   DataInceput = x.DataInceput,
-                   DataSfarsit = x.DataSfarsit,
-                   StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
-                   Id = x.Id
+                if (dataInceput <= dataFinal)
+                {
+                    listaReturnata = _context.Concedius.Include(x => x.Angajat)
+                   .Include(x => x.StareConcediu)
+                   .Include(x => x.TipConcediu)
+                   .Where(x => (x.DataInceput.Date >= dataInceput.Date && x.DataInceput.Date <= dataFinal.Date
+                                        && x.DataSfarsit.Date >= dataInceput.Date && x.DataSfarsit.Date <= dataFinal.Date && x.Angajat.Manager.Id == id))
 
-               })
-               .ToList();
+                   .Select(x => new Concediu
+                   {
+                       Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume, Id = x.Angajat.Manager.Id } },
+                       TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                       Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                       DataInceput = x.DataInceput,
+                       DataSfarsit = x.DataSfarsit,
+                       StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                       Id = x.Id
+
+                   })
+                   .ToList();
+                }
+                return listaReturnata;
+                
             }
-            return listaReturnata;
+            else
+            {
+                
+                  if (dataInceput <= dataFinal)
+                    {
+                        listaReturnata = _context.Concedius.Include(x => x.Angajat)
+                       .Include(x => x.StareConcediu)
+                       .Include(x => x.TipConcediu)
+                       .Where(x => (x.DataInceput.Date >= dataInceput.Date && x.DataInceput.Date <= dataFinal.Date
+                                            && x.DataSfarsit.Date >= dataInceput.Date && x.DataSfarsit.Date <= dataFinal.Date))
+
+                       .Select(x => new Concediu
+                       {
+                           Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                           TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                           Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                           DataInceput = x.DataInceput,
+                           DataSfarsit = x.DataSfarsit,
+                           StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                           Id = x.Id
+
+                       })
+                       .ToList();
+                    }
+                    return listaReturnata;
+                
+            }
         }
 
         [HttpGet("GetConcediiByTipConcediuId")]
-        public List<Concediu> GetConcediiByTipConcediuId(int tipConcediuId)
+        public List<Concediu> GetConcediiByTipConcediuId(int tipConcediuId, bool esteAdmin, int id)
         {
-            return _context.Concedius.Include(x => x.Angajat)
+            if (esteAdmin == false)
+            {
+                return _context.Concedius.Include(x => x.Angajat)
              .Include(x => x.StareConcediu)
              .Include(x => x.TipConcediu).Where(x => x.TipConcediuId == tipConcediuId)
              .Select(x => new Concediu
              {
-                 Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                 Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume, Id = x.Angajat.Manager.Id } },
                  TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
                  Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
                  DataInceput = x.DataInceput,
@@ -141,7 +258,29 @@ namespace Concediu_WebApi.Controllers
                  Id = x.Id
 
              })
+             .Where(x => x.Angajat.Manager.Id == id)
              .ToList();
+            }
+            else
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+            .Include(x => x.StareConcediu)
+            .Include(x => x.TipConcediu).Where(x => x.TipConcediuId == tipConcediuId)
+            .Select(x => new Concediu
+            {
+                Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                DataInceput = x.DataInceput,
+                DataSfarsit = x.DataSfarsit,
+                StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                Id = x.Id
+
+            })
+           
+            .ToList();
+            }
+;
         }
 
 
@@ -153,29 +292,57 @@ namespace Concediu_WebApi.Controllers
 
 
         [HttpGet("GetConcediiDupaFiltre")]
-        public List<Concediu> GetConcediiDupaFiltre(string? nume, int? stareId, int? tipId, DateTime? dataInceput, DateTime? dataFinal)
+        public List<Concediu> GetConcediiDupaFiltre(string? nume, int? stareId, int? tipId, DateTime? dataInceput, DateTime? dataFinal, bool esteAdmin, int id)
         {
-            return _context.Concedius.Include(x => x.Angajat)
-             .Include(x => x.StareConcediu)
-             .Include(x => x.TipConcediu)
-             .Where(x => nume == null || x.Angajat.Nume.Contains(nume))
-             .Where(x => stareId !=0? x.StareConcediuId == stareId:true)
-             .Where(x => tipId!=0?x.TipConcediuId == tipId:true)
-             .Where(x => (dataInceput != null && dataFinal!= null)?
-             (x.DataInceput.Date >= dataInceput.Value.Date && x.DataInceput.Date <= dataFinal.Value.Date
-                                    && x.DataSfarsit.Date >= dataInceput.Value.Date && x.DataSfarsit.Date <= dataFinal.Value.Date):true)
-             .Select(x => new Concediu
-             {
-                 Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
-                 TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
-                 Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
-                 DataInceput = x.DataInceput,
-                 DataSfarsit = x.DataSfarsit,
-                 StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
-                 Id = x.Id
+           if (esteAdmin == false)
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+            .Include(x => x.StareConcediu)
+            .Include(x => x.TipConcediu)
+            .Where(x => nume == null || x.Angajat.Nume.Contains(nume))
+            .Where(x => stareId != 0 ? x.StareConcediuId == stareId : true)
+            .Where(x => tipId != 0 ? x.TipConcediuId == tipId : true)
+            .Where(x => (dataInceput != null && dataFinal != null) ?
+            (x.DataInceput.Date >= dataInceput.Value.Date && x.DataInceput.Date <= dataFinal.Value.Date
+                                   && x.DataSfarsit.Date >= dataInceput.Value.Date && x.DataSfarsit.Date <= dataFinal.Value.Date) : true)
+            .Select(x => new Concediu
+            {
+                Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume, Id = x.Angajat.Manager.Id } },
+                TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                DataInceput = x.DataInceput,
+                DataSfarsit = x.DataSfarsit,
+                StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                Id = x.Id
 
-             })
-             .ToList();
+            })
+            .Where(x => x.Angajat.Manager.Id == id)
+            .ToList();
+            }
+            else
+            {
+                return _context.Concedius.Include(x => x.Angajat)
+            .Include(x => x.StareConcediu)
+            .Include(x => x.TipConcediu)
+            .Where(x => nume == null || x.Angajat.Nume.Contains(nume))
+            .Where(x => stareId != 0 ? x.StareConcediuId == stareId : true)
+            .Where(x => tipId != 0 ? x.TipConcediuId == tipId : true)
+            .Where(x => (dataInceput != null && dataFinal != null) ?
+            (x.DataInceput.Date >= dataInceput.Value.Date && x.DataInceput.Date <= dataFinal.Value.Date
+                                   && x.DataSfarsit.Date >= dataInceput.Value.Date && x.DataSfarsit.Date <= dataFinal.Value.Date) : true)
+            .Select(x => new Concediu
+            {
+                Angajat = new Angajat { Nume = x.Angajat.Nume, Prenume = x.Angajat.Prenume, Manager = new Angajat { Nume = x.Angajat.Manager.Nume, Prenume = x.Angajat.Manager.Prenume } },
+                TipConcediu = new TipConcediu { Nume = x.TipConcediu.Nume },
+                Inlocuitor = new Angajat { Nume = x.Inlocuitor.Nume, Prenume = x.Inlocuitor.Prenume },
+                DataInceput = x.DataInceput,
+                DataSfarsit = x.DataSfarsit,
+                StareConcediu = new StareConcediu { Nume = x.StareConcediu.Nume },
+                Id = x.Id
+
+            })
+            .ToList();
+            }
         }
 
     }
